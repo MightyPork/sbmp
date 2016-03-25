0dda2132a3d1ab388c5e244e1604954bad54e200
Ondřej Hruška <ondra@ondrovo.com>
2016-03-25 18:17:19 +0100

    merged master

--- library/sbmp_bulk.c
+++ library/sbmp_bulk.c
@@ -45,7 +45,7 @@ bool FLASH_FN sbmp_bulk_abort(SBMP_Endpoint *ep, uint16_t sesn)
 	// 0-byte response is just the header.
 	bool suc = sbmp_ep_start_response(ep, DG_BULK_ABORT, 0, sesn);
 
-	if (suc) sbmp_dbg("Bulk transfer aborted; sesn %"PRIu16, sesn);
+	if (suc) sbmp_dbg("Bulk transfer aborted; sesn %d", sesn);
 	return suc;
 }
 
